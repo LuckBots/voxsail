cf27d9d056c909944f1ac7e320477f07eede3c4f
Dear.cp <75558435+LuckBots@users.noreply.github.com>
2026-08-06 10:17:31 +0800

    feat: 增加字幕检查公开接口

---
+++ packages/subtitle-qc/src/index.js
@@ -0,0 +1,3 @@
+export { parseSubtitle, parseTimestamp, SubtitleParseError, visibleCharacterCount } from './parser.js';
+export { DEFAULT_RULES, summarizeQuality, validateSubtitles } from './validator.js';
+export { buildReport, formatTextReport } from './reporter.js';
